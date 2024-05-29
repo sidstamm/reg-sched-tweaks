157eeca8dd7458d3402ee1518c61ff9d81946e56
Sid Stamm <stammsl@rose-hulman.edu>
2024-05-29 16:08:10 -0400

    make a Me button for search

--- reg-sched-tweaks-source/reg-sched-functions.js
+++ reg-sched-tweaks-source/reg-sched-functions.js
@@ -11,7 +11,7 @@
  *     -- executed once, on load.  Will not persist.
  *  > added ability to press "enter" in search form to trigger username, room, or course lookup
  *
- * @version 1.0 - 5/30/2023
+ * @version 1.0.1 - 5/29/2024
  * @author Sid Stamm <stammsl@rose-hulman.edu>
  ***************************/
 
@@ -32,6 +32,18 @@ const GROUP_SELECTION_INSTRUCTIONS = `
 // some helpers
 let QS = (sel) => document.querySelector(sel);
 let QSA = (sel) => document.querySelectorAll(sel);
+
+// constructs a button with a map of attributes, a list of classes, and a click handler.
+function buttonMaker(attr_map, class_list, onClickHandler) {
+  let btn = document.createElement("input");
+  btn.setAttribute("type", "button");
+  for (const k in attr_map) { btn.setAttribute(k, attr_map[k]); }
+  for (const c of class_list) { btn.classList.add(c); }
+  if (onClickHandler) {
+    btn.addEventListener("click", onClickHandler);
+  }
+  return btn;
+}
 
 /* Adds UI to select items in the "ad-hoc group schedule" thing.
  * Also adds show/hide unselected items.
@@ -106,9 +118,6 @@ if (QS("select#id6")) {
 /* Tweaks for main page only */
 if(QS("input[name=id1]")) {
 
-  // Sets the default focus to be the username (main search page only)
-  QS("input[name=id1]").focus();
-
   // Removes the initial focus requirement on the lookup buttons.
   // This way you don't have to click into "room" before clicking the room search
   // button (for example).
@@ -119,7 +128,6 @@ if(QS("input[name=id1]")) {
 
   // Fix enter key for room and courseid lookup submission
   function fixEnter(id, btn) {
-    console.log("Adding enter listener to id " + id)
     QS("input[name="+id+"]").addEventListener("keyup",
         (ev) => { if (ev.key == "Enter") { QS("input[name="+btn+"]").click(); }});
   }
@@ -128,26 +136,36 @@ if(QS("input[name=id1]")) {
   if(QS("input[name=id4]")) { fixEnter("id4", "bt4"); }
   if(QS("input[name=id5]")) { fixEnter("id5", "bt5"); }
 
+
   // add "me" option (push to store your username, push to use it)
-  btn = document.createElement("input");
-  btn.setAttribute("type", "button");
-  btn.setAttribute("value", "+");
-  btn.setAttribute("title", "click to set a value that will be remembered when you visit this page");
-  btn.setAttribute("onClick",
-    `localStorage.setItem("regsched.Me", prompt("what is your rhit username?"))`);
-  QS("input[name=bt1]").insertAdjacentElement("afterend", btn);
+  mebtn = buttonMaker(
+    {"name": "mebtn", "value":"me", "title":localStorage.getItem("regsched.Me")},
+    ["tweaked"],
+    (e) => {
+      let v = localStorage.getItem("regsched.Me");
+      if (v == null) {
+        alert("Click the '+' button first.");
+        return false;
+      }
+      QS("input[name=id1]").value = v;
+      QS("input[name=bt1]").click();
+    }
+  );
 
-  btn = document.createElement("input");
-  btn.setAttribute("type", "button");
-  btn.setAttribute("value", "me");
-  btn.setAttribute("title", localStorage.getItem("regsched.Me"));
-  let inp = QS("input[name=id1]");
-  let inp_btn = QS("input[name=bt1]");
-  btn.setAttribute("onClick",
-    `document.querySelector("input[name=id1]").value = localStorage.getItem("regsched.Me");
-     document.querySelector("input[name=bt1]").click();`);
+  btn = buttonMaker(
+    {"name": "meplusbtn", "value": "+", "title": "click to set a value that will be remembered when you visit this page"},
+    ["tweaked"],
+    (e) => {
+      def = prompt("What is your rhit username?  This will be remembered for you.", localStorage.getItem("regsched.Me") || "");
+      if (def) {
+        localStorage.setItem("regsched.Me", def);
+        QS("input[name=id1]").value = localStorage.getItem("regsched.Me");
+      }
+    }
+  );
   QS("input[name=bt1]").insertAdjacentElement("afterend", btn);
+  QS("input[name=bt1]").insertAdjacentElement("afterend", mebtn);
 
+  // Sets the default focus to be the username (main search page only)
+  QS("input[name=id1]").focus();
 }
-
-
